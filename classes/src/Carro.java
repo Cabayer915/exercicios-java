aa5c999de4a660dfbac1bc21bde40ac335b3fa7d
Cabayer915 <cauaribeirob915@gmail.com>
2024-05-19 21:18:40 -0300

    Adicionando o Exercicio de Metodos e Atributos

--- classes/src/Carro.java
+++ classes/src/Carro.java
@@ -2,6 +2,71 @@ public class Carro {
     private Integer numeracao;
     private String modelo;
     private String cor;
+    private Integer capacidadeTanque;
     private String placa;
     private String marca;
+
+    public Carro(Integer numeracao, String modelo, String cor, Integer capacidadeTanque, String placa, String marca) {
+        this.numeracao = numeracao;
+        this.modelo = modelo;
+        this.cor = cor;
+        this.capacidadeTanque = capacidadeTanque;
+        this.placa = placa;
+        this.marca = marca;
+    }
+
+    public Carro() {
+    }
+
+    public Integer getNumeracao() {
+        return numeracao;
+    }
+
+    public void setNumeracao(Integer numeracao) {
+        this.numeracao = numeracao;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Integer getCapacidadeTanque() {
+        return capacidadeTanque;
+    }
+
+    public void setCapacidadeTanque(Integer capacidadeTanque) {
+        this.capacidadeTanque = capacidadeTanque;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public Integer calculaCapacidadeTanque(Integer valor) {
+        return capacidadeTanque * valor;
+    }
 }
